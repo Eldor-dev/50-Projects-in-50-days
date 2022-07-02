e2020b845c5f4e18758ae11b2845a78ae99dded1
Eldor Azimov <eldorazimov37@gmail.com>
2022-07-02 22:00:53 +0900

    Form Wave

--- 7.Form wave/script.js
+++ 7.Form wave/script.js
@@ -3,6 +3,9 @@ const labels = document.querySelectorAll(".form-control label");
 labels.forEach((label) => {
   label.innerHTML = label.innerText
     .split("")
-    .map((letter) => `<span>${letter}</span>`)
+    .map(
+      (letter, inx) =>
+        `<span style="transition-delay: ${inx * 50}ms">${letter}</span>`
+    )
     .join("");
 });
