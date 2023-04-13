4808d5d0b0832f24841dcca3d1b10866ab871324
Eldor Azimov <eldorazimov37@gmail.com>
2023-04-13 21:00:28 +0900

    Background Slider

---
+++ 18.Background Slider/script.js
@@ -0,0 +1,29 @@
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rigthBtn = document.getElementById("right");
+
+let activeSlide = 1;
+
+rigthBtn.addEventListener("click", () => {
+  activeSlide++;
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+
+  setBgToBody();
+  setActiveClass();
+});
+
+setBgToBody();
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveClass() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+
+  slides[activeSlide].classList.add("actve");
+}
