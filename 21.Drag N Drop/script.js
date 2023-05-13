1eb6bec16b6b35501a407252d84a898c87069c5a
Eldor Azimov <eldorazimov37@gmail.com>
2023-05-13 21:05:52 +0900

    Drag N Drop

--- 21.Drag N Drop/script.js
+++ 21.Drag N Drop/script.js
@@ -4,26 +4,39 @@ const empties = document.querySelectorAll(".empty");
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
 function dragStart() {
-  console.log("drag start");
+  this.className += " hold";
+  setTimeout(() => {
+    this.className = "invisible";
+  }, 0);
 }
 
 function dragEnd() {
-  console.log("drag end");
+  this.className = "fill";
 }
 
-function dragOver() {
+function dragOver(e) {
+  e.preventDefault();
   console.log("drag over");
 }
 
-function dragEnter() {
-  console.log("drag enter");
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
 }
 
 function dragLeave() {
-  console.log("drag leave");
+  this.className = "empty";
 }
 
-function dragDrop() {
-  console.log("drag drop");
+function dragDrop(e) {
+  e.className = "empty";
+  this.append(fill);
 }
